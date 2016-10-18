994f58c96242aef1ddf2a53be4a8269d7f98937e
Jerald Sheets <jsheetsjr@paypal.com>
2016-10-18 09:27:05 -0700

    remediating dashboard installation

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,19 +38,4 @@ node default {
   # Example:
   #   class { 'my_class': }
 
-class {'dashboard':
-  dashboard_ensure          => 'present',
-  dashboard_dbhost          => 'localhost',
-  dashboard_dbport          => '3306',
-  dashboard_user            => 'puppet-dbuser',
-  dashboard_group           => 'puppet-dbgroup',
-  dashboard_password        => 'changeme',
-  dashboard_db              => 'dashboard_prod',
-  dashboard_charset         => 'utf8',
-  dashboard_site            => $fqdn,
-  dashboard_port            => '8080',
-  mysql_root_pw             => 'changemetoo',
-  passenger                 => true,
-}
-
 }
